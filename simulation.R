61028defdccd9796c76928f3d3bd9fa606ef1fe8
andbe <and.be.like@gmail.com>
2018-06-13 14:09:33 +0100

    Create simulation.R

---
+++ simulation.R
@@ -0,0 +1,48 @@
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+
+stratA = function(keys = 50, locks = 7){
+  keys = 1:keys
+  locks = sample(keys, locks)
+  lock = sample(locks, 1, replace = FALSE)
+  trials = 1
+  for(i in keys){
+    if(i == lock) return(trials)
+    trials = trials + 1
+  }
+}
+
+stratB = function(keys = 50, locks = 7){
+  keys = 1:keys
+  locks = sample(keys, locks, replace = FALSE)
+  trials = 1
+  for(i in keys){
+    for(j in locks){
+      if(i == j) return(trials)
+      trials = trials + 1
+    }
+  }
+}
+
+trialsA = c()
+trialsB = c()
+keys = 50
+locks = 7
+n.sims = 10000
+
+for(t in 1:n.sims){
+  trialsA = c(trialsA, stratA())
+  trialsB = c(trialsB, stratB())
+}
+
+df = data.frame(
+  "trials" = c(trialsA, trialsB),
+  "strategy" = factor(c(rep("A",n.sims),rep("B",n.sims)))
+  )
+
+
+ggplot(df, aes(x = strategy, y = trials, col = strategy)) + geom_boxplot()
+
+df %>% filter(strategy == "A") %>% summary
+df %>% filter(strategy == "B") %>% summary
